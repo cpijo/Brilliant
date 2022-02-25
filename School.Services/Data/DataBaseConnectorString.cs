00e36f8f87b46194f6e4bee20ebcb157ace8adf9
Sphiwe <Siphiwem@Multitier.co.za>
2022-02-25 14:57:12 +0200

    busy coding

--- School.Services/Data/DataBaseConnectorString.cs
+++ School.Services/Data/DataBaseConnectorString.cs
@@ -10,8 +10,8 @@ namespace NetcarePortal.Models.Data
     public class DataBaseConnectorString
     {
         public static string urlHost = ConfigurationManager.AppSettings["urlHost"];
-        public static string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
-        public static readonly string connectionStringAzure = ConfigurationManager.ConnectionStrings["connectionStringAzure"].ConnectionString;
+        //private static string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+        //public static readonly string connectionStringAzure = ConfigurationManager.ConnectionStrings["connectionStringAzure"].ConnectionString;
         public static string ConnectionString
         {
             get
@@ -22,7 +22,8 @@ namespace NetcarePortal.Models.Data
                 }
                 else
                 {
-                    return ConfigurationManager.ConnectionStrings["connectionStringAzure"].ConnectionString;
+                    // spijoserversql' requested by the login. Client with IP address '197.229.140.120'  use the Windows Azure Management Portal or run sp_set_firewall_rule 
+                     return ConfigurationManager.ConnectionStrings["connectionStringAzure"].ConnectionString;
                 }
             }
         }
