215be4952edc30b061d97568129894b332892be6
Sphiwe <Siphiwem@Multitier.co.za>
2022-02-25 20:43:42 +0200

    still coding ....

---
+++ School.Services/Data/sql_Connection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Services.Data
+{
+    class sql_Connection
+    {
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+            SqlDataReader dataReader;
+            string sql = null;
+
+            connetionString = "Data Source=ServerName;Initial Catalog=DatabaseName;Integrated Security=SSPI;User ID=UserName;Password=Password";
+            sql = "Your SQL Statement Here , like Select * from product";
+            connection = new SqlConnection(connetionString);
+            try
+            {
+                connection.Open();
+                command = new SqlCommand(sql, connection);
+                dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                   // MessageBox.Show(dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + " - " + dataReader.GetValue(2));
+                }
+                dataReader.Close();
+                command.Dispose();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+               // MessageBox.Show("Can not open connection ! ");
+            }
+        }
+    }
+}
